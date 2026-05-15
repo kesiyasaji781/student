3f1dfaccc60bd665e6569e0aea4b1bf92e295682
kesiya <kesiyasaji781@gmail.com>
2026-05-15 16:08:10 +0530

    update

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = FastAPI(title="Student Management API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://student-app-self-kappa.vercel.app/"],
+    allow_origins=["http://localhost:5173","https://student-app-self-kappa.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
